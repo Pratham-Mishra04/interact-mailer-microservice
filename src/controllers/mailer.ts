567494785f581fabe4c79def919e803b07df7cef
ishita-babar <ishitavasant.babar2023@vitstudent.ac.in>
2024-06-18 21:10:29 +0530

    feat: invitation project, groupchat, organisation

--- src/controllers/mailer.ts
+++ src/controllers/mailer.ts
@@ -164,7 +164,7 @@ const getParamFuncFromReq = (
                     '{{Organization.title}}',
                     organization.title || '');
             case 17:
-                parameterizedHTML?.replace(
+                return parameterizedHTML?.replace(
                     '{{GroupChat.title}}',
                     groupchat.title || ''
                 )
