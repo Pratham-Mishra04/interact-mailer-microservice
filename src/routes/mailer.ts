7a589e4807bc6f43a2f13c2175b982d7422fe442
TejasGhate <tejasghatte5@gmail.com>
2024-06-29 19:14:28 +0530

    fix: fixed route path

--- src/routes/mailer.ts
+++ src/routes/mailer.ts
@@ -6,7 +6,7 @@ import { blockProd, protect } from '../middlewares/protect';
 const Router = express.Router();
 
 Router.post('/api', protect, sendMail);
-Router.post('/api/sendMultipleMail', protect, sendMultipleMail)
+Router.post('/api/multiple', protect, sendMultipleMail)
 Router.get('/token', blockProd, createSendToken);
 
 export default Router;
